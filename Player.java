c5a725149bea6b21ca2c1f1aa9d3bf4892a60183
astxrfr <147904124+astxrfr@users.noreply.github.com>
2026-06-05 18:46:51 -0600

    Agregar clase Bullet y Gun para implementar mecánica de disparo en el juego

--- Player.java
+++ Player.java
@@ -4,11 +4,13 @@ public class Player extends Actor
 {
     private static Player PLAYER_INSTANCE;
 
-    // Declarar la velocidad del jugador
     private int speed = 4;
+
+    private final Gun gun;
 
     private Player() {
         PLAYER_INSTANCE = this;
+        this.gun = new Gun();
     }
 
     public static Player get() {
@@ -19,6 +21,7 @@ public class Player extends Actor
     // Acciones principales
     public void act() 
     {
+        fireGun();
         movePlayer();
         collectKey();
         enterDoor();
@@ -28,6 +31,18 @@ public class Player extends Actor
         GameWorld world = (GameWorld)getWorld();
         world.removeObject(this);
         world.unloadWorld();
+    }
+
+    @Override
+    protected void addedToWorld(World world) {
+        super.addedToWorld(world);
+        world.addObject(gun, this.getX(), this.getY());
+    }
+
+    private void fireGun() {
+        if (Greenfoot.mouseClicked(getWorld())) {
+            gun.shoot();
+        }
     }
 
     //Movimiento principal
@@ -43,18 +58,17 @@ public class Player extends Actor
         double length = Math.sqrt(dx*dx + dy*dy);
         if (length > 0) { dx /= length; dy /= length; }
 
-        int x = this.getX();
-        int y = this.getY();
+        double x = this.getX();
+        double y = this.getY();
 
         if (getOneObjectAtOffset((int)dx*speed*2, (int)dy*speed*2, CollisionObject.class) == null) {
             x += (int)(dx * speed);
             y += (int)(dy * speed);
         }
 
-        this.setLocation(x, y);
+        this.setLocation((int)x, (int)y);
     }
-    
-    //Comer gusanos
+
     private void collectKey()
     {
         Actor key = getOneObjectAtOffset(0, 0, Key.class);
@@ -79,5 +93,51 @@ public class Player extends Actor
         if(world.getCounter().getScore() < 3) return;
 
         world.nextWorld();
+    }
+
+    /**
+     * Write a description of class Player.Gun here.
+     *
+     * @author (your name)
+     * @version (a version number or a date)
+     */
+    public class Gun extends Actor
+    {
+        private final String gunTexture = "rocket.png";
+
+        protected Gun() {
+            render();
+        }
+
+        protected void shoot() {
+            MouseInfo mouse = Greenfoot.getMouseInfo();
+            if (mouse == null) return;
+
+            int targetX = mouse.getX();
+            int targetY = mouse.getY();
+
+            Bullet bullet = new Bullet(targetX, targetY);
+            getWorld().addObject(bullet, this.getX(), this.getY());
+        }
+
+        public void act()
+        {
+            this.setLocation(PLAYER_INSTANCE.getX(), PLAYER_INSTANCE.getY());
+            updateOrientation();
+        }
+
+        private void render() {
+            this.setImage(gunTexture);
+        }
+
+        private void updateOrientation() {
+            MouseInfo mouse = Greenfoot.getMouseInfo();
+            if (mouse == null) return;
+
+            int targetX = mouse.getX();
+            int targetY = mouse.getY();
+
+            this.turnTowards(targetX, targetY);
+        }
     }
 }
